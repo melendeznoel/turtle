e0dfd5e7297c224179f91d2005779cfbcabe06d3
oatcoder <noelmelendez@gmail.com>
2024-06-29 23:36:50 -0500

    UPDATE
    - loading css

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { useLocation} from 'react-router-dom'
+import {useLocation} from 'react-router-dom'
 
-function App () {
-  const location = useLocation()
+function App() {
+    const [loaded, setLoaded] = useState(false)
 
-  const {pathname} = location
+    useEffect(() => {
+        const location = useLocation()
 
-  console.log(pathname)
+        const {pathname} = location
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer">
-          Learning React
-        </a>
-      </header>
-    </div>
-  )
+        console.log(pathname)
+
+        import('')
+            .then(() => {
+                setLoaded(true)
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link"
+                   href="https://reactjs.org"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                    Learning React
+                </a>
+            </header>
+        </div>
+    )
 }
 
 export default App
