08e3efe79d218895521655775c63b14bf263282d
oatcoder <noelmelendez@gmail.com>
2024-06-29 20:37:56 -0500

    UPDATE
    - added router
    - adding location handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { useLocation} from 'react-router-dom'
 
-function App() {
+function App () {
+  const location = useLocation()
+
+  const {pathname} = location
+
+  console.log(pathname)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer">
           Learning React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
